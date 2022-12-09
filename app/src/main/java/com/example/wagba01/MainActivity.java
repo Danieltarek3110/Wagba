b82b5d759aac79025f10c29917e1132cd53af953
DanielTarek3110 <danieltarek3110@gmail.com>
2022-12-09 02:34:51 +0200

    Add intents

--- app/src/main/java/com/example/wagba01/MainActivity.java
+++ app/src/main/java/com/example/wagba01/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.wagba01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.wagba01.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    Intent intent01 , intent02;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        intent01 = new Intent(this, SignUp.class);
+        intent02 = new Intent(this, LogIn.class);
+
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent01);
+
+            }
+        });
+
+        binding.btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent02);
+
+            }
+        });
+
     }
 }
