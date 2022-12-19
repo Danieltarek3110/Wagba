d55376f99ae896f7c8afb1b9bd7a752b3c449326
DanielTarek3110 <danieltarek3110@gmail.com>
2022-12-19 14:16:26 +0200

    Cleaned code

--- app/src/main/java/com/example/wagba01/Restaurants.java
+++ app/src/main/java/com/example/wagba01/Restaurants.java
@@ -40,85 +40,90 @@ public class Restaurants extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
-    private void SetUpRestaurantModels(){
-        String[] RestaurantModelsNames = getResources().getStringArray(R.array.RestaurantsNames);
-       // SetUpDishesModel();
+
+    private void SetUpRestaurantModels() {
+        // String[] RestaurantModelsNames = getResources().getStringArray(R.array.RestaurantsNames);
+        // SetUpDishesModel();
 
         ArrayList<Dishes_Model> kfc_menu = new ArrayList<Dishes_Model>();
-        kfc_menu.add(new Dishes_Model("Dinner Box" , "EGP120" , dishesImages[0] ));
-        kfc_menu.add(new Dishes_Model("Rizo Combo" , "EGP80" , dishesImages[1] ));
-        kfc_menu.add(new Dishes_Model("Mighty Zinger" , "EGP100" , dishesImages[2] ));
-        kfc_menu.add(new Dishes_Model("Bucket 21pcs" , "EGP350" , dishesImages[3] ));
-        restaurant_models_array.add(new Restaurant_model("KFC" , RestaurantsImages[0] , kfc_menu));
+        kfc_menu.add(new Dishes_Model("Dinner Box", "EGP120", dishesImages[0]));
+        kfc_menu.add(new Dishes_Model("Rizo Combo", "EGP80", dishesImages[1]));
+        kfc_menu.add(new Dishes_Model("Mighty Zinger", "EGP100", dishesImages[2]));
+        kfc_menu.add(new Dishes_Model("Bucket 21pcs", "EGP350", dishesImages[3]));
+        restaurant_models_array.add(new Restaurant_model("KFC", RestaurantsImages[0], kfc_menu));
 
 
         ArrayList<Dishes_Model> mac_menu = new ArrayList<Dishes_Model>();
-        mac_menu.add(new Dishes_Model("Cheese Burger" , "120EGP" , dishesImages[4]));
-        mac_menu.add(new Dishes_Model("Chicken Macdo" , "240EGP" , dishesImages[5]));
-        mac_menu.add(new Dishes_Model("Fries" , "100EGP" , dishesImages[6]));
-        mac_menu.add(new Dishes_Model("Apple Pie" , "80EGP" , dishesImages[7]));
-        restaurant_models_array.add(new Restaurant_model("McDonald's" , RestaurantsImages[1] , mac_menu));
+        mac_menu.add(new Dishes_Model("Cheese Burger", "120EGP", dishesImages[4]));
+        mac_menu.add(new Dishes_Model("Chicken Macdo", "240EGP", dishesImages[5]));
+        mac_menu.add(new Dishes_Model("Fries", "100EGP", dishesImages[6]));
+        mac_menu.add(new Dishes_Model("Apple Pie", "80EGP", dishesImages[7]));
+        restaurant_models_array.add(new Restaurant_model("McDonald's", RestaurantsImages[1], mac_menu));
 
         ArrayList<Dishes_Model> Papa_menu = new ArrayList<Dishes_Model>();
-        Papa_menu.add(new Dishes_Model("Chicken Ranch " , "120" , dishesImages[8]));
-        Papa_menu.add(new Dishes_Model("Peperoni" , "240" , dishesImages[9]));
-        Papa_menu.add(new Dishes_Model("Chicken Ranch Large" , "290" , dishesImages[10]));
-        Papa_menu.add(new Dishes_Model("Chicken Ranch Family" , "320" , dishesImages[11]));
-        restaurant_models_array.add(new Restaurant_model("Papa John's" , RestaurantsImages[2] , Papa_menu));
+        Papa_menu.add(new Dishes_Model("Chicken Ranch ", "120", dishesImages[8]));
+        Papa_menu.add(new Dishes_Model("Peperoni", "240", dishesImages[9]));
+        Papa_menu.add(new Dishes_Model("Chicken Ranch Large", "290", dishesImages[10]));
+        Papa_menu.add(new Dishes_Model("Chicken Ranch Family", "320", dishesImages[11]));
+        restaurant_models_array.add(new Restaurant_model("Papa John's", RestaurantsImages[2], Papa_menu));
 
 
         ArrayList<Dishes_Model> BKing_menu = new ArrayList<Dishes_Model>();
-        BKing_menu.add(new Dishes_Model("Whooper" , "120" , dishesImages[12]));
-        BKing_menu.add(new Dishes_Model("Double Whooper" , "240" , dishesImages[13]));
-        BKing_menu.add(new Dishes_Model("Triple Whooper" , "290" , dishesImages[14]));
-        BKing_menu.add(new Dishes_Model("Cheese Burger" , "80" , dishesImages[15]));
-        restaurant_models_array.add(new Restaurant_model("Burger King" , RestaurantsImages[3] , BKing_menu));
+        BKing_menu.add(new Dishes_Model("Whooper", "120", dishesImages[12]));
+        BKing_menu.add(new Dishes_Model("Double Whooper", "240", dishesImages[13]));
+        BKing_menu.add(new Dishes_Model("Triple Whooper", "290", dishesImages[14]));
+        BKing_menu.add(new Dishes_Model("Cheese Burger", "80", dishesImages[15]));
+        restaurant_models_array.add(new Restaurant_model("Burger King", RestaurantsImages[3], BKing_menu));
 
 
         ArrayList<Dishes_Model> Starbucks_menu = new ArrayList<Dishes_Model>();
-        Starbucks_menu.add(new Dishes_Model("Caramel Coffee" , "20" , dishesImages[16]));
-        Starbucks_menu.add(new Dishes_Model("Latte" , "40" , dishesImages[17]));
-        Starbucks_menu.add(new Dishes_Model("Tea" , "90" , dishesImages[18]));
-        Starbucks_menu.add(new Dishes_Model("Strawberry Coffee" , "120" , dishesImages[19]));
-        restaurant_models_array.add(new Restaurant_model("Starbucks" , RestaurantsImages[4] , Starbucks_menu));
+        Starbucks_menu.add(new Dishes_Model("Caramel Coffee", "20", dishesImages[16]));
+        Starbucks_menu.add(new Dishes_Model("Latte", "40", dishesImages[17]));
+        Starbucks_menu.add(new Dishes_Model("Tea", "90", dishesImages[18]));
+        Starbucks_menu.add(new Dishes_Model("Strawberry Coffee", "120", dishesImages[19]));
+        restaurant_models_array.add(new Restaurant_model("Starbucks", RestaurantsImages[4], Starbucks_menu));
 
         ArrayList<Dishes_Model> Sizzler = new ArrayList<Dishes_Model>();
-        Sizzler.add(new Dishes_Model("Well-done" , "120" , dishesImages[20]));
-        Sizzler.add(new Dishes_Model("Medium well" , "140" , dishesImages[21]));
-        Sizzler.add(new Dishes_Model("Medium rare" , "190" , dishesImages[22]));
-        Sizzler.add(new Dishes_Model("Rare" , "220" , dishesImages[23]));
-        restaurant_models_array.add(new Restaurant_model("Sizzler" , RestaurantsImages[5] , Sizzler));
+        Sizzler.add(new Dishes_Model("Well-done", "120", dishesImages[20]));
+        Sizzler.add(new Dishes_Model("Medium well", "140", dishesImages[21]));
+        Sizzler.add(new Dishes_Model("Medium rare", "190", dishesImages[22]));
+        Sizzler.add(new Dishes_Model("Rare", "220", dishesImages[23]));
+        restaurant_models_array.add(new Restaurant_model("Sizzler", RestaurantsImages[5], Sizzler));
 
 
         ArrayList<Dishes_Model> Hardees = new ArrayList<Dishes_Model>();
-        Hardees.add(new Dishes_Model("Mushroom Burger" , "120" , dishesImages[24]));
-        Hardees.add(new Dishes_Model("Chicken Burger" , "140" , dishesImages[25]));
-        Hardees.add(new Dishes_Model("Beef Burger" , "190" , dishesImages[26]));
-        Hardees.add(new Dishes_Model("Ham Burger" , "220" , dishesImages[27]));
-        restaurant_models_array.add(new Restaurant_model("Hardees" , RestaurantsImages[6] , Hardees));
+        Hardees.add(new Dishes_Model("Mushroom Burger", "120", dishesImages[24]));
+        Hardees.add(new Dishes_Model("Chicken Burger", "140", dishesImages[25]));
+        Hardees.add(new Dishes_Model("Beef Burger", "190", dishesImages[26]));
+        Hardees.add(new Dishes_Model("Ham Burger", "220", dishesImages[27]));
+        restaurant_models_array.add(new Restaurant_model("Hardees", RestaurantsImages[6], Hardees));
 
 
         ArrayList<Dishes_Model> Costa = new ArrayList<Dishes_Model>();
-        Costa.add(new Dishes_Model("Turkish Coffee" , "20" , dishesImages[28]));
-        Costa.add(new Dishes_Model("American Coffee" , "40" , dishesImages[29]));
-        Costa.add(new Dishes_Model("Latte" , "50" , dishesImages[30]));
-        Costa.add(new Dishes_Model("Cappuccino" , "60" , dishesImages[31]));
-        restaurant_models_array.add(new Restaurant_model("Costa" , RestaurantsImages[7] , Costa));
+        Costa.add(new Dishes_Model("Turkish Coffee", "20", dishesImages[28]));
+        Costa.add(new Dishes_Model("American Coffee", "40", dishesImages[29]));
+        Costa.add(new Dishes_Model("Latte", "50", dishesImages[30]));
+        Costa.add(new Dishes_Model("Cappuccino", "60", dishesImages[31]));
+        restaurant_models_array.add(new Restaurant_model("Costa", RestaurantsImages[7], Costa));
 
 
         ArrayList<Dishes_Model> Hut = new ArrayList<Dishes_Model>();
-        Hut.add(new Dishes_Model("Cheese Lovers" , "120" , dishesImages[32]));
-        Hut.add(new Dishes_Model("Super Supreme" , "140" , dishesImages[33]));
-        Hut.add(new Dishes_Model("Chicken Ranch" , "190" , dishesImages[34]));
-        Hut.add(new Dishes_Model("Family Pizza" , "220" , dishesImages[35]));
-        restaurant_models_array.add(new Restaurant_model("Pizza Hut" , RestaurantsImages[8] , Hut));
+        Hut.add(new Dishes_Model("Cheese Lovers", "120", dishesImages[32]));
+        Hut.add(new Dishes_Model("Super Supreme", "140", dishesImages[33]));
+        Hut.add(new Dishes_Model("Chicken Ranch", "190", dishesImages[34]));
+        Hut.add(new Dishes_Model("Family Pizza", "220", dishesImages[35]));
+        restaurant_models_array.add(new Restaurant_model("Pizza Hut", RestaurantsImages[8], Hut));
 
         ArrayList<Dishes_Model> Attack = new ArrayList<Dishes_Model>();
-        Attack.add(new Dishes_Model("Mushroom Burger" , "120" , dishesImages[36]));
-        Attack.add(new Dishes_Model("Chicken Burger" , "140" , dishesImages[37]));
-        Attack.add(new Dishes_Model("Beef Burger" , "190" , dishesImages[38]));
-        Attack.add(new Dishes_Model("Heart Attack Brgr" , "220" , dishesImages[39]));
-        restaurant_models_array.add(new Restaurant_model("Heart Attack" , RestaurantsImages[9] , Attack));
+        Attack.add(new Dishes_Model("Mushroom Burger", "120", dishesImages[36]));
+        Attack.add(new Dishes_Model("Chicken Burger", "140", dishesImages[37]));
+        Attack.add(new Dishes_Model("Beef Burger", "190", dishesImages[38]));
+        Attack.add(new Dishes_Model("Heart Attack Brgr", "220", dishesImages[39]));
+        restaurant_models_array.add(new Restaurant_model("Heart Attack", RestaurantsImages[9], Attack));
+    }
+
+
+}
 
 
 /*
@@ -128,7 +133,7 @@ public class Restaurants extends AppCompatActivity {
         }
     }
 
- */ }
+ */
 /*
     private void SetUpDishesModel(){
         String[] dishesNames = getResources().getStringArray(R.array.DishNames);
@@ -139,4 +144,3 @@ public class Restaurants extends AppCompatActivity {
 
     }
 */
-}
